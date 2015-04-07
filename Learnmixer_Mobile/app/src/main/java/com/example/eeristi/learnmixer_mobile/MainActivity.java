b1f3ff48579fd48e5140fbf50931ef94b6d26941
ezgiyamac <ezgiyamac@yahoo.com.tr>
2015-04-07 05:16:28 +0300

    Add AlertDialog for incorrect username and password

--- Learnmixer_Mobile/app/src/main/java/com/example/eeristi/learnmixer_mobile/MainActivity.java
+++ Learnmixer_Mobile/app/src/main/java/com/example/eeristi/learnmixer_mobile/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.eeristi.learnmixer_mobile;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.app.AlertDialog;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -20,6 +22,8 @@ public class MainActivity extends ActionBarActivity {
         Button my_btn_login = (Button) findViewById(R.id.btn_login);
         final EditText my_username = (EditText) findViewById(R.id.edt_txt_username);
         final EditText my_password = (EditText) findViewById(R.id.edt_txt_password);
+        final AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+
 
         my_btn_login.setOnClickListener(new View.OnClickListener()
         {
@@ -29,6 +33,21 @@ public class MainActivity extends ActionBarActivity {
                 if (my_username.getText().toString().equals("ezgi") && my_password.getText().toString().equals("yamac") )
                 {
                     startActivity(new Intent("com.example.eeristi.learnmixer_mobile.SCREEN_TWO"));
+                }
+                else
+                {
+
+                    alertDialog.setTitle("Incorrect...");
+                    alertDialog.setMessage("Please control your username and password!..");
+                    alertDialog.setButton("OK", new DialogInterface.OnClickListener()
+                    {
+                        public void onClick(DialogInterface dialog, int which)
+                        {
+                            // here you can add functions
+                        }
+                    });
+                    alertDialog.setIcon(R.drawable.alert);
+                    alertDialog.show();
                 }
 
 
