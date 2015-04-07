00419db246422000ad4e5af517be3a06ea4f3947
ezgiyamac <ezgiyamac@yahoo.com.tr>
2015-04-07 05:37:46 +0300

    Add Listview object for select course

--- Learnmixer_Mobile/app/src/main/java/com/example/eeristi/learnmixer_mobile/screen2.java
+++ Learnmixer_Mobile/app/src/main/java/com/example/eeristi/learnmixer_mobile/screen2.java
@@ -1,16 +1,44 @@
 package com.example.eeristi.learnmixer_mobile;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class screen2 extends ActionBarActivity
+public class screen2 extends ListActivity
 {
+    String my_course_list_array []={"Math","Language(English)","Physics"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.screen2);
+
+        setListAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,my_course_list_array));
 
     }
+
+
+
+    @Override
+    protected  void onListItemClick(ListView lv, View v,int position, long id)
+    {
+        if(position==0)
+        {
+            startActivity(new Intent("com.example.eeristi.learnmixer_mobile.SCREEN_TWO"));
+        }
+        else if(position==1)
+        {
+            startActivity(new Intent("com.example.eeristi.learnmixer_mobile.SCREEN_TWO"));
+        }
+
+        super.onListItemClick(lv, v, position, id);
+
+    }
+
+
 
 }
